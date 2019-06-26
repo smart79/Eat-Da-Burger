fcf324e74de2967efcf620551c1d428b23ac38da
Chris Martinez <smartinez79@protonmail.com>
2019-06-26 14:29:55 -0500

    update to seeds

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Hamburger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Cheeseburger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Soy burger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Turkey burger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Chicken burger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Veggie burger');
