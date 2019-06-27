dd384861926568e796ffdc1fee6605bff81f3671
Chris Martinez <smartinez79@protonmail.com>
2019-06-27 06:10:10 -0500

    update to ORM.js

--- config/orm.js
+++ config/orm.js
@@ -24,4 +24,36 @@ function objToSql(ob) {
 
         arr.push(key + "=" + value);
     }
+    return arr.toString();
 }
+// Object for all our SQL statement functions
+var orm = {
+    all: function (tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    create: function (table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
