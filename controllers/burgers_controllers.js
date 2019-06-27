0b78462bfc7d769a024a4d12f1e5c4ea61f3cc02
Chris Martinez <smartinez79@protonmail.com>
2019-06-27 10:08:44 -0500

    update to get route in burgers_controller.js

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -7,3 +7,10 @@ var burger = require("../models/burger.js");
 router.get("/", function (req, res) {
     res.redirect("/burgers");
 });
+router.get("/burgers", function (req, res) {
+    // express callback response by calling burger.all(selectAllBurgers)
+    burger.all(function (burgerData) {
+        // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
+        res.render("index", { burger_data: burgerData });
+    });
+});
