4d82cb888f7f6141ab745c7af95cf9e6abeac147
Chris Martinez <smartinez79@protonmail.com>
2019-06-27 10:10:59 -0500

    update to put route in burgers_controller.js

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -14,3 +14,12 @@ router.get("/burgers", function (req, res) {
         res.render("index", { burger_data: burgerData });
     });
 });
+// put route -> back to index
+router.put("/burgers/:id", function (req, res) {
+    burger.update(req.params.id, function (result) {
+        // wrapper for orm.js that using MySQL update callback will return a log to console,
+        // render back to index with handle
+        console.log(result);
+        // Send back response and let page reload from .then in Ajax
+        res.sendStatus(200);
+    });
